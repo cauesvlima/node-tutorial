e2520d1a3bc4846be0c702ed4ce822a9e3f7c664
Cauê Lima <cauesvlima@gmail.com>
2024-09-16 22:27:21 -0300

    routes

---
+++ rest/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const rotaProdutos = require('./routes/produtos')
+
+app.use('/produtos', rotaProdutos);
+app.use('/teste',( req, res, next)=>{
+    res.status(200).send({
+        mensagem:'OK, Deu certo'
+    });
+});
+module.exports = app;
