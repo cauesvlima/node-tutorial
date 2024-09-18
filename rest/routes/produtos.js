a21b5574ce87fcc1ee6cf7861ffe1313a06c99e1
Cauê Lima <cauesvlima@gmail.com>
2024-09-18 14:53:59 -0300

    documentado responses

--- rest/routes/produtos.js
+++ rest/routes/produtos.js
@@ -9,10 +9,25 @@ router.get('/', (req, res, next)=>{
         
         conn.query(
             'SELECT * FROM produtos',
-            (error, resultado, field)=>{
+            (error, result, field)=>{
                 conn.release();
                 if(error){res.status(500).send({error:error})}
-                return res.status(200).send({response:resultado});
+                const response = {
+                    quantidade: result.length,
+                    produto:result.map(prod =>{
+                        return{
+                            idprodutos:prod.idprodutos,
+                            nome:prod.nome,
+                            preco:prod.preco,
+                            request:{
+                                tipo:'GET',
+                                descricao:'Retorna um produto específico',
+                                url:`${process.env.LOCAL_URL}produtos/${prod.idprodutos}`
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send(response);
             }
         )
     })
@@ -28,10 +43,20 @@ router.post('/',(req, res, next)=>{
             (error, resultado, field)=>{
                 conn.release();
                 if(error){res.status(500).send({error:error})};
-                res.status(201).send({
+                const response = {
                     mensagem:'Produto inserido com sucesso!',
-                    produtoCriado: resultado.insertId
-                });
+                    produtoCriado:{
+                        id_produto:resultado.insertId,
+                        nome:req.body.nome,
+                        preco:req.body.preco,
+                        request:{
+                            tipo:'GET',
+                            descricao:'Retorna todos os produtos',
+                            url:`${process.env.LOCAL_URL}produtos/`
+                        }
+                    }
+                }
+                res.status(201).send(response);
             }
         )
     })
@@ -41,13 +66,28 @@ router.post('/',(req, res, next)=>{
 router.get('/:id_produto', (req, res, next)=>{
         mysql.getConnection((error, conn)=>{
             if(error){return res.status(500).send({error:error})};
+
             conn.query(
                 'SELECT * FROM produtos WHERE idprodutos = ?;',
                 [req.params.id_produto],
                 (error, resultado, field)=>{
                     conn.release();
                     if(error){res.status(500).send({error:error})}
-                    return res.status(200).send({response:resultado});
+                    
+            if (resultado.length == 0) {return res.status(404).send({mensagem:'Não foi encontrado nenhum produto com esse ID'})}
+            const response ={
+                produto: {
+                    id_produto: resultado[0].idprodutos,
+                    nome: resultado[0].nome,
+                    preco: resultado[0].preco,
+                    request:{
+                        tipo:'GET',
+                        descricao:'Retorna um produto específico',
+                        url:`${process.env.LOCAL_URL}produtos/${resultado[0].idprodutos}`
+                    }
+                }
+            }
+                    return res.status(200).send(response);
                 }
             )
         })
@@ -62,12 +102,23 @@ router.patch('/', (req, res, next)=>{
                 preco = ?
                 WHERE idprodutos =?`,
             [req.body.nome, req.body.preco, req.body.idprodutos],
-            (error, resultado, field)=>{
+            (error, result, field)=>{
                 conn.release();
                 if(error){res.status(500).send({error:error})};
-                res.status(201).send({
+                const response = {
                     mensagem:'Produto alterado com sucesso!',
-                });
+                    produtoAtualizado:{
+                        id_produto:req.body.idprodutos,
+                        nome:req.body.nome,
+                        preco:req.body.preco,
+                        request:{
+                            tipo:'GET',
+                            descricao:'Retorna todos os produtos',
+                            url:`${process.env.LOCAL_URL}produtos/` + req.body.idprodutos
+                        }
+                    }
+                }
+                res.status(201).send(response);
             }
         )
     })
